3eedea01a874e3a31e13659df55d4b7481e55d5c
Ken0723 <43611876+Ken0723@users.noreply.github.com>
2025-10-29 02:14:48 +0800

    CI/CD Automation Attempt 1
    - implement CI/CD automation to this project

---
+++ scripts/start_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APP_DIR="/home/ec2-user/flask-chatbot/personal-chatbot"
+SERVICE_FILE="flaskapp.service"
+
+echo "Stopping existing Flask service..."
+systemctl stop flaskapp || true
+
+echo "Copying new Systemd service file..."
+cp "$APP_DIR/$SERVICE_FILE" /etc/systemd/system/$SERVICE_FILE
+
+echo "Setting permissions..."
+chown -R ec2-user:ec2-user "$APP_DIR"
+
+echo "Reloading systemd daemon..."
+systemctl daemon-reload
+
+echo "Enabling and starting Flask service..."
+systemctl enable flaskapp
+systemctl start flaskapp
+
+echo "Status check..."
+systemctl status flaskapp
