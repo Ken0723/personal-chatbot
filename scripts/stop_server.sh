0a8d2840a77b0873e751792ba93f84361b6a20fa
Ken0723 <43611876+Ken0723@users.noreply.github.com>
2025-10-30 03:37:05 +0800

    Update stop_server.sh
    - try to fix exit code 1

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 sudo systemctl stop flaskapp || true
 sudo systemctl disable flaskapp || true
 
-PID=$(sudo lsof -t -i:8086)
+PID=$(lsof -t -i:8086)
 
 if [ -n "$PID" ]; then
     echo "Found rogue process on port 8086 (PID: $PID). Killing process..."
-    sudo kill -9 $PID
+    sudo kill -9 $PID || true
     sleep 2
     echo "Process killed."
 else
     echo "No rogue processes found on port 8086."
 fi
+
+exit 0
