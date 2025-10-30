89045bb64b5114cd9bcf7ce15e52e94398b986b2
Ken0723 <43611876+Ken0723@users.noreply.github.com>
2025-10-30 17:58:39 +0800

    Update clean_install_dir.sh
    - Delete find port process

--- scripts/clean_install_dir.sh
+++ scripts/clean_install_dir.sh
@@ -4,17 +4,6 @@ set -ex
 systemctl stop flaskapp  || true
 systemctl disable flaskapp || true
 
-PID=$(lsof  -t -i:8086 2>/dev/null)
-
-if [ -n "$PID" ]; then
-    echo "Found rogue process on port 8086 (PID: $PID). Killing process..."
-    kill -9 $PID 2 || true
-    sleep 2
-    echo "Process killed."
-else
-    echo "No rogue processes found on port 8086."
-fi
-
 TARGET_DIR="/home/ec2-user/flask-chatbot/personal-chatbot"
 
 if [ -d "$TARGET_DIR" ]; then
